7de2a875029d43d21c3e31bc377ba999699f036a
vthakkar226 <vthakkar226@gmail.com>
2023-05-02 10:26:03 +0530

    02_05_2023_home_2

---
+++ 1456_ Maximum Number of Vowels in a Substring of Given Length.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    s = "abciiidef"
+    k = 3
+    obj = Solution()
+    print(obj.maxVowels(s,k))
