04827eb0d355cf7a98b12fd60509bdee335118c7
= <=>
2023-05-11 20:50:39 +0530

    11_05_2023_office_2

---
+++ 841. Keys and Rooms.py
@@ -0,0 +1,27 @@
+import unittest
+
+class Solution:
+    def canVisitAllRooms(self, rooms) -> bool:
+        pass
+
+
+
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.obj = Solution()
+        return super().setUp()
+    
+    def test_case1(self):
+        self.assertEqual(self.obj.canVisitAllRooms([[1],[2],[3],[]]),True,"Fail")
+
+    def test_case2(self):
+        self.assertEqual(self.obj.canVisitAllRooms([[1,3],[3,0,1],[2],[0]]),False,"Fail")
+    
+    def test_case2(self):
+        self.assertEqual(self.obj.predictPartyVictory("RRDRDDD"),"Radiant","Fail")
+
+    
+if __name__ == "__main__":
+    unittest.main()
